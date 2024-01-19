0b8117325675e3330abdbc22f3e31708d963fe34
BRUNNO <brunnoml@gmail.com>
2024-01-18 22:00:15 -0300

    Update app.py
    Write code to import flask  and solved problem to find python path

---
+++ app.py
@@ -0,0 +1,9 @@
+# Importação
+# Pode apresentar problema na importação do flask, então procura a versão do python digitando pip --version e corrige o PATH para a versão informada no terminal
+
+from flask import Flask
+
+# Instanciar o aplicativo do Flask
+
+app = Flask(__name__)
+
