5594edb37d6e99b0cc9286b910a65158ca995860
BRUNNO <brunnoml@gmail.com>
2024-01-19 00:05:44 -0300

    add flask_sqlalchemy and class Product

--- app.py
+++ app.py
@@ -2,10 +2,25 @@
 # Pode apresentar problema na importação do flask, então procura a versão do python digitando pip --version e corrige o PATH para a versão informada no terminal
 
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 # Instanciar o aplicativo do Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecomemerce.dp'
+
+# Começar a utilizar o nosso banco de dados
+
+db = SQLAlchemy(app)
+
+
+# Modelagem
+# Produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
@@ -16,4 +31,4 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
